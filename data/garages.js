99acdca5cea55d5b4d36ab0a2af61df6113bd220
jiya-dhiman <jiyadhiman07@gmail.com>
2025-09-20 13:58:08 -0400

    Create garages.js

---
+++ data/garages.js
@@ -0,0 +1,18 @@
+export const garages = [
+  {
+    id: 'grant',
+    name: 'Grant Street Garage',
+    capacity: 200,
+    available: 47,
+    location: { latitude: 40.4237, longitude: -86.9212 },
+    trend: { '8am': 0.6, '12pm': 0.9, '5pm': 0.3 }
+  },
+  {
+    id: 'university',
+    name: 'University Street Garage',
+    capacity: 300,
+    available: 112,
+    location: { latitude: 40.4261, longitude: -86.9153 },
+    trend: { '8am': 0.4, '12pm': 0.7, '5pm': 0.2 }
+  }
+];
